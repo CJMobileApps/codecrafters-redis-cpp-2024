b563e44fc4c5b57ac96afa13ef0c8bfffd40d806
Chris Jones <chris@cjmobileapps.com>
2024-12-13 00:53:54 -0600

    Respond to PING : Part 1

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,8 +7,68 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <vector>
+
+void createServer(int server_fd) {
+    sockaddr_in client_addr{};
+    socklen_t client_addr_len = sizeof(client_addr); // Correct type for length
+    std::cout << "Waiting for a client to connect...\n";
+
+    // Log message for debugging
+    std::cout << "Logs from your program will appear here!\n";
+
+    // Correct parentheses in the `accept` condition
+    int new_socket = accept(server_fd, (sockaddr *) &client_addr, &client_addr_len);
+    if (new_socket < 0) {
+        perror("Accept Failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Client connected\n";
+
+    // Buffer for reading data
+    char buffer[1024] = {};
+
+    // Read data from the client
+    const ssize_t bytes_received = read(new_socket, buffer, sizeof(buffer) - 1);
+    if (bytes_received < 0) {
+        perror("Read Failed");
+        close(new_socket);
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Null-terminate and print the data received
+    buffer[bytes_received] = '\0';
+    std::cout << "Received from client: " << buffer << "\n";
+
+    // Write and send response to the client
+    std::string response = "+PONG\r\n";
+   const char *c_response = response.c_str();
+
+    const ssize_t writeRequestClient = write(new_socket, c_response, response.size());
+    if (writeRequestClient < 0) {
+        perror("Send Response failed");
+        close(new_socket);
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    // printf("Response sent to client\n%s\n", response);
+    std::cout << "Response sent to client: " << response << "\n";
+
+
+    // Close the client socket
+    close(new_socket);
+
+    // Close the server socket
+    close(server_fd);
+}
+
 
 int main(int argc, char **argv) {
+    // To test run command -> echo -ne '*1\r\n$4\r\nping\r\n' | nc localhost 6379
+
+
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -32,6 +92,7 @@ int main(int argc, char **argv) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(6379);
 
+    // Bind the socket to the network address port
     if (bind(server_fd, (sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
         std::cerr << "Failed to bind to port 6379\n";
         return 1;
@@ -43,17 +104,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    sockaddr_in client_addr{};
-    int client_addr_len = sizeof(client_addr);
-    std::cout << "Waiting for a client to connect...\n";
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
-
-    accept(server_fd, (sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    std::cout << "Client connected\n";
-
-    close(server_fd);
+    createServer(server_fd);
 
     return 0;
 }
